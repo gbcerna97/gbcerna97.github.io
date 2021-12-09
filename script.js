bef0857219363be802faf01d3ef71a3a128b9e0a
gbcerna97 <87927073+gbcerna97@users.noreply.github.com>
2021-12-09 11:24:04 +0800

    Edit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const arabicInput = document.getElementById("arabicInput");
+const numInput = document.getElementById("numInput");
 
-arabicInput.addEventListener("input",(e)=>{
-  romanInput.value = arabicToRoman(e.target.value);
+numInput.addEventListener("input",(e)=>{
+  romanOutput.value = convertToRoman(e.target.value);
 });
 
-function arabicToRoman(number){
+function convertToRoman(number){
   let roman = "";
   const romanNumList = {M:1000,CM:900, D:500,CD:400, C:100, XC:90,L:50, XV: 40, X:10, IX:9, V:5, IV:4, I:1};
   let a;
