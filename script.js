f221c2fbcc14a237bd4bc07f49454a40d80b7b43
gbcerna97 <87927073+gbcerna97@users.noreply.github.com>
2021-12-09 10:45:31 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const arabicInput = document.getElementById("arabicInput");
+
+arabicInput.addEventListener("input",(e)=>{
+  romanInput.value = arabicToRoman(e.target.value);
+});
+
+function arabicToRoman(number){
+  let roman = "";
+  const romanNumList = {M:1000,CM:900, D:500,CD:400, C:100, XC:90,L:50, XV: 40, X:10, IX:9, V:5, IV:4, I:1};
+  let a;
+  if(number < 1 || number > 3999) //Checks is the input number is less than 1 or greater that 3999
+    return "Enter a number between 1 and 3999";
+  else{
+    for(let key in romanNumList){ //If the input number is greater than or equal than 1 or less than or equal than 3999, the conversion will then take place
+        a = Math.floor(number / romanNumList[key]);
+        if(a >= 0){
+            for(let i = 0; i < a; i++){
+              roman += key;
+            }
+          }
+        number = number % romanNumList[key];
+    }
+  }
+
+  return roman;
+}
